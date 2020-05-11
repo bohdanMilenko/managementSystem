48a0b6fd25a47371f056183a9f23122aafe8e6f5
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-05-11 19:45:08 -0400

    New custom exception for Services

---
+++ src/main/java/com/managementSystem/exception/ServiceException.java
@@ -0,0 +1,40 @@
+package com.managementSystem.exception;
+
+public class ServiceException extends  RuntimeException{
+
+    public ServiceException() {
+        super();
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+
+    @Override
+    public synchronized Throwable getCause() {
+        return super.getCause();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+    }
+}
